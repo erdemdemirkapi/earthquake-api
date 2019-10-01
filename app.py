ef3a8019bc522145e9e2ce76f75e26c9c199d3b7
erdem <demirkapierdem@gmail.com>
2019-10-01 13:32:04 +0300

    json response completed

--- app.py
+++ app.py
@@ -4,18 +4,23 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def earthquakes():
     URL = "http://www.koeri.boun.edu.tr/scripts/lst7.asp"
-    COLUMNS = ["Tarih", "Saat", "Enlem", "Boylam", "Derinlik", "MD", "ML", "Mw", "Yer", "Çözüm Niteliği"]
+    COLUMNS = ["Tarih", "Saat", "Enlem(N)", "Boylam(E)", "Derinlik(km)", "MD", "ML", "Mw", "Yer", "Çözüm Niteliği"]
     html = urllib.request.urlopen(URL).read()
 
-    data = BeautifulSoup(html)
-    pre = data.find("pre")
-    data = str(pre).split("\n")
+    pre = BeautifulSoup(html).find("pre")
+    pre_array = str(pre).split("\n")
+    data = []
+    for i in range(7, len(pre_array) - 2):
+        detail = pre_array[i].split()
+        detail[8:-1] = [" ".join(detail[8:-1])]
+        detail_json = dict(zip(COLUMNS, detail))
+        data.append(detail_json)
 
-    return data
+    response = {"data" : data}
+    return response
 
 if __name__ == '__main__':
     app.run()
